bb9caba72019e85c4619b4d4388c8e0530068b13
Martin Lopez <90067541+MartinLopez01@users.noreply.github.com>
2024-06-17 16:31:19 -0700

    Add files via upload

---
+++ TecnologiadeComponentes/src/App.js
@@ -0,0 +1,27 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import CustomInput from "./CustomInput";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nuevo Usuario</h1>
+        <CustomInput label="Nombre" disabled={false} type="text"/>
+        <CustomInput label="Correo" disabled={false} type="text"/>
+        <CustomInput label="Contraseña" disabled={false} type="password"/>
+        <div className="Contenedor">
+        <div>
+          <input type="checkbox" disabled={false}/>
+          <label>Aceptar termins y condiciones</label>
+        </div>
+          <Button disabled={false}>Cancelar</Button>
+          <Button disabled={false}>Enviar</Button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
